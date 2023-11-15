fe51ce892159a8235c52b43b246d0eb8acd3e99c
Vamsi Madhav <vmcse09@gmail.com>
2023-11-15 20:29:26 +0530

    Added redirect to success.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-const username = "vmcse09";
+const username = "test";
 const password = process.argv[2];
 
 const url = `mongodb+srv://${username}:${password}@cluster0.ijopi3a.mongodb.net/?retryWrites=true&w=majority`;
